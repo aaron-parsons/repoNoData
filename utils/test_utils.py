c9ae82695f668e452b862021aa7ad63d00c9d481
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2017-02-28 09:27:01 +0000

    added some test utils and loader tests

---
+++ utils/test_utils.py
@@ -0,0 +1,9 @@
+'''
+some utils for the test
+'''
+
+
+
+def get_test_data_path(fname):
+    path = "/dls/science/users/clb02321/DAWN_stable/GithubLFS/repo4data/test_data/%s"
+    return path % fname
