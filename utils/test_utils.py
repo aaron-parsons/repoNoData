e39e57c29417d8bc03dd68a46e6fb34f93eea031
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2017-02-28 16:15:31 +0000

    changes how we look for the test data repo

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -5,6 +5,5 @@ some utils for the test
 import inspect
 
 def get_test_data_path(fname):
-    print "The repo is in are in ", inspect.stack()[0][1].split('repoNoData')[0]
-    path = "/dls/science/users/clb02321/DAWN_stable/GithubLFS/repo4data/test_data/%s"
+    path = inspect.stack()[0][1].split('repoNoData')[0]+'repo4data/test_data/%s'
     return path % fname
