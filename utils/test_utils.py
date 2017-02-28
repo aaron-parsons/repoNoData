23589e803462a54dbb4e101ec520cf3da79ca25a
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2017-02-28 15:41:57 +0000

    ok maybe this will

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -2,9 +2,9 @@
 some utils for the test
 '''
 
-import sys
+import inspect
 
 def get_test_data_path(fname):
-    print "The repo is in are in ",sys.argv[0].split('repoNoData')[0]
+    print "The repo is in are in ", inspect.stack()[0][1].split('repoNoData')[0]
     path = "/dls/science/users/clb02321/DAWN_stable/GithubLFS/repo4data/test_data/%s"
     return path % fname
